ed010ecd9dc17b1d8ea6de16f12b2a38e7f0f071
Ahmad Rifai <93535625+Ahmadrif4520@users.noreply.github.com>
2025-11-10 21:28:18 +0700

    Update auth-check.js

--- assets/js/auth-check.js
+++ assets/js/auth-check.js
@@ -1,5 +1,22 @@
-auth.onAuthStateChanged(user => {
-    if (user) {
-        window.location.href = "kpi_dashboard.html";
+import { auth, db } from "./firebase-config.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+import { doc, getDoc } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
+
+onAuthStateChanged(auth, async (user) => {
+  if (!user) {
+    window.location.href = "login.html";
+    return;
+  }
+
+  const docRef = doc(db, "users", user.uid);
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+    const data = docSnap.data();
+    console.log("Role user:", data.role);
+
+    if (data.role === "admin") {
+      document.getElementById("admin-section").style.display = "block";
     }
+  }
 });
